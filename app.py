610e16ba42427f6b116275015e45bf7c83d6fa32
rana biswas <rana.biswas.office@gmail.com>
2023-03-23 18:17:05 +0600

    crud

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ ma = Marshmallow()
 mysql = MySQL(app)
 
 app.config['SECRET_KEY'] = 'seCREtKeY'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost:3308/flask_api'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost:3306/flask_app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
@@ -64,6 +64,43 @@ def customer_insert():
     return jsonify({"Message": "New Customer Added"})
 
         
+@app.route('/get_customer/<id>', methods=['GET'])
+def customer_byid(id):
+    customer = []
+    data = Customers.query.get(id)
+    if data is None:
+        return jsonify('No Such Customer Found')
+    customer = customer_schema.dump(data)  #need to use customer_schema not customers_schema
+    return jsonify(customer)       
+
+
+@app.route('/customer_update/<id>', methods = ['PUT'])
+def customer_upd(id):
+    data = Customers.query.get(id)
+    _json = request.json
+    customer_name = _json['customer_name']
+    email = _json['email']
+    phone = _json['phone']
+    customer_type = _json['customer_type']
+    country_id = _json['country_id']
+    c_address = _json['c_address']
+    c_bin_nid = _json['c_bin_nid']
+    c_tin = _json['c_tin']
+    shipping_country_id = _json['shipping_country_id']
+    shipping_address = _json['shipping_address']
+    new_customer = data(customer_name=customer_name, email=email, phone=phone, customer_type=customer_type, country_id=country_id, 
+                            c_address=c_address, c_bin_nid=c_bin_nid, c_tin=c_tin, shipping_country_id=shipping_country_id, shipping_address=shipping_address)
+    db.session.update(new_customer)
+    db.session.commit()
+    return jsonify({"Message": "New Customer Update"})
+
+
+@app.route('/get_customer/delete/<id>', methods=['DELETE'])
+def customer_delete_byid(id):
+    data = Customers.query.get(id)
+    db.session.delete(data)
+    db.session.commit()
+    return jsonify('The Customer has been deleted')  
 
 
 @app.route('/product')
